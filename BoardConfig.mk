11de4fa83f84eea58faeb6eef22bca354ba36f18
Abir Hasan AHK <ahksoft33@gmail.com>
2026-07-28 22:59:16 +0600

    Fix kernel: skip kernel build, no prebuilt kernel available

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,7 @@ TARGET_SCREEN_DENSITY := 560
 TARGET_USES_VULKAN := true
 
 # Kernel
+TARGET_NO_KERNEL := true
 BOARD_BOOTIMG_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_CMDLINE := dyndbg=&#34;func alloc_contig_dump_pages +p&#34; earlycon=exynos4210,0x10A00000 console=ttySAC0,115200 androidboot.console=ttySAC0 printk.devkmsg=on cma_sysfs.experimental=Y swiotlb=noforce at24.write_timeout=100 log_buf_len=1024K bootconfig
@@ -60,17 +61,12 @@ BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-TARGET_KERNEL_CONFIG := raven_defconfig
-TARGET_KERNEL_SOURCE := kernel/google/raven
 
-# Kernel - prebuilt
-TARGET_FORCE_PREBUILT_KERNEL := true
-ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
-BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
-BOARD_INCLUDE_DTB_IN_BOOTIMG := 
-endif
+# Kernel - prebuilt (disabled: no kernel prebuilt available, stock kernel used)
+# TARGET_FORCE_PREBUILT_KERNEL := true
+# TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+# TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+# BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
